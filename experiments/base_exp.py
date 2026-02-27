2d0b0b349204fb9fa0369798dba2844136611046
Isaac Houngue <i.houngue@instadeep.com>
2026-02-27 17:46:45 +0200

    Delete experiements folder

--- experiments/base_exp.py
+++
@@ -1,3 +0,0 @@
-class BaseExperiment:
-    def track():
-        pass
--- experiments/muon_exp.py
+++
@@ -1,5 +0,0 @@
-from .base_export import BaseExperiment
-
-class MuonExperiment(BaseExperiment):
-    def track(self):
-        raise NotImplementedError
